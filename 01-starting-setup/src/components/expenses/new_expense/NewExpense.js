f9cb365c3e6e27b084e4eee04d7cb9da7c9529e1
Anlun Jiang <anlunjiang@outlook.com>
2021-12-15 15:41:38 +0000

    pop up expense adder

--- 01-starting-setup/src/components/expenses/new_expense/NewExpense.js
+++ 01-starting-setup/src/components/expenses/new_expense/NewExpense.js
@@ -1,18 +1,27 @@
 import "./NewExpense.css"
 import ExpenseForm from "../expense_form/ExpenseForm";
-
+import {useState} from "react";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false)
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       id: Math.random().toString(),
       ...enteredExpenseData,
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+  const startEditinHandler = () => {
+    setIsEditing(true)
+  };
+  const stopEditinHandler = () => {
+    setIsEditing(false)
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {!isEditing && <button onClick={startEditinHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onStopEditing={stopEditinHandler}/>}
     </div>
   )
 }
